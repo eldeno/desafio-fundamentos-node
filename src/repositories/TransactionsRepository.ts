e60941920fc0a34c3e115f9d9b32b0d0f2a51748
Angelo <angelo.chagas@live.com>
2020-08-25 17:37:19 -0300

    Features: List/Add transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,11 @@
 import Transaction from '../models/Transaction';
+// import transactionRouter from '../routes/transaction.routes';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +21,70 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // const incomeTransactions = this.transactions.reduce((acc, cur) => {
+    //   const transactionType = 'income';
+    //   if (cur.type === transactionType) {
+    //     acc.push(cur);
+    //   }
+    //   return acc;
+    // }, []);
+
+    // const outcomeTransactions = this.transactions.reduce((acc, cur) => {
+    //   const transactionType = 'outcome';
+    //   if (cur.type === transactionType) {
+    //     acc.push(cur);
+    //   }
+    //   return acc;
+    // }, []);
+
+    // const totalIncome = incomeTransactions.reduce((acc, cur) => {
+    //   return acc + cur.value;
+    // }, 0);
+
+    // const totalOutcome = outcomeTransactions.reduce((acc, cur) => {
+    //   return acc + cur.value;
+    // }, 0);
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
